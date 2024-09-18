6181e21fae68080d648922530bb0265035ab685a
AnoratoMarcela <marcelayngridanorato@gmail.com>
2024-09-18 16:10:18 -0300

    Subindo a hacker edition

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 // Função que soma dois números
-function somar() {}
+function somar(n1, n2) {
+  return n1 + n2
+}
+console.log(somar(2,2));
 
 // Função que subtrai dois números
-function subtrair() {}
+function subtrair(n1,n2) {
+  return n1 - n2
+}
+console.log(subtrair(2,2))
 
 // Função que multiplica dois números
-function multiplicar() {}
+function multiplicar(n1,n2) {
+  return n1 * n2
+}
+console.log(multiplicar(2,2));
+
 
 // Função que divide dois números, com verificação de divisão por zero
-function dividir() {}
+function dividir(n1,n2) {
+  return n1/n2
+}
+console.log(dividir(2,2));
+
 
 // Função que verifica se um número é par
-function ehPar() {}
+function ehPar(numero) {
+  return numero % 2 === 0
+}
+console.log(ehPar(2,2));
+
 
 // Exporta todas as funções para serem usadas nos testes
 module.exports = {
@@ -20,5 +38,4 @@ module.exports = {
   multiplicar,
   dividir,
   ehPar,
-  encontrarMaior,
 };
